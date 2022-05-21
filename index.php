19c56849aa251723ea7adce34357e6fa1f79476c
nashkIhaysnairfAilfaRdammahuM <nashkihaysnairfailfardammahum@gmail.com>
2022-05-21 18:12:35 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello, world!</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita tempora quisquam voluptatum necessitatibus repellat aliquam optio quibusdam asperiores rerum odit, velit, porro dolorem ea ullam adipisci fugit officia aut et?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
